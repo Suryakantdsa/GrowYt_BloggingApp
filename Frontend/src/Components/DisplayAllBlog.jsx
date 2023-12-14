190785d9508b8050a6de3af69758c10b1e8b2b71
Suryakantdsa <dassuryakantadas@gmail.com>
2023-12-14 16:28:23 +0530

    fix some key issue

--- Frontend/src/Components/DisplayAllBlog.jsx
+++ Frontend/src/Components/DisplayAllBlog.jsx
@@ -10,26 +10,28 @@ const DisplayAllBlog = () => {
 
   return (
     <div className="px-10 flex items-center flex-col">
-      {!blogData ? (
-        // If blogData is null (loading state), show shimmer
-        <Shimmer />
-      ) : blogData.length > 0 ? (
-        // If blogData has items, display BlogCards
-        blogData.map((blog) => (
-          <div className="p-4 w-full flex flex-col border border-orange-400 shadow-xl rounded-md my-4 hover:bg-slate-100">
-          <Link to={`/blog/${blog._id}`} key={blog._id}>
-            <BlogCard blog={blog}  key={blog._id}/>
+    {!blogData ? (
+          // If blogData is null (loading state), show shimmer
+      <Shimmer />
+    ) : blogData.length > 0 ? 
+     // If blogData has items, display BlogCards
+    (
+      blogData.map((blog) => (
+        <div key={blog._id} className="p-4 w-full flex flex-col border border-orange-400 shadow-xl rounded-md my-4 hover:bg-slate-100">
+          <Link to={`/blog/${blog._id}`}>
+            <BlogCard blog={blog} />
           </Link>
-          </div>
-        ))
-      ) : (
-        // If blogData is empty, show a message
-        <NoBlogFound />
-      )}
-      {blogData?.length > 0 && (
-        <h1 className="text-3xl font-bold text-center">That's All ..!!🙂</h1>
-      )}
-    </div>
+        </div>
+      ))
+    ) :
+       // If blogData is empty, show a message
+    (
+      <NoBlogFound />
+    )}
+    {blogData?.length > 0 && (
+      <h1 className="text-3xl font-bold text-center">That's All ..!!🙂</h1>
+    )}
+  </div>
   );
 };
 
