bb3d879586aaf204214ef19fa95840c0a6be332d
Suryakantdsa <dassuryakantadas@gmail.com>
2023-12-15 16:10:48 +0530

    fix some issue

--- Backend/Index.js
+++ Backend/Index.js
@@ -5,12 +5,12 @@ const { app } = require("./app.js");
 dotenv.config({
     path: './.env'
 });
-
+const PORT=8000
 // mongodb connection
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
-            console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        app.listen(8000, () => {
+            console.log(`⚙️ Server is running at port : ${PORT}`);
         });
     })
     .catch((err) => {
--- Backend/db/dbConnection.js
+++ Backend/db/dbConnection.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const uri="mongodb+srv://Suryakant:Suryadas@cluster0.mydbwj6.mongodb.net/BloggingApp?retryWrites=true&w=majority"
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+        const connectionInstance = await mongoose.connect(uri);
         console.log("Mongodb Connected Successfully ..!!");
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
